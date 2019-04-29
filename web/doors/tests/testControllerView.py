cea2be2708ea3184d830461db8e0be912427a74b
Andrey Valyaev <dron.valyaev@gmail.com>
2019-04-29 23:40:19 +0300

    #90: Fix tests

--- web/doors/tests/testControllerView.py
+++ web/doors/tests/testControllerView.py
@@ -5,17 +5,18 @@
 
 from django.test import TestCase
 from doors.models import Controller
+import json
 
 
 class ControllerViewTest(TestCase):
 	def testNoControllers(self):
 		response = self.client.get('/controller/')
 		self.assertEqual(response.status_code, 200)
-		self.assertIn('<?xml', response.content.decode('utf8'))
 
 	def testConcreteController(self):
 		Controller.objects.create(address='3.2.3.2', port='7777')
 		response = self.client.get('/controller/')
 		self.assertEqual(response.status_code, 200)
-		self.assertIn('address>3.2.3.2</address', response.content.decode('utf8'))
-		self.assertIn('port>7777</port', response.content.decode('utf8'))
+		rep = json.loads(response.content.decode('utf8'))
+		self.assertEqual(rep['controllers'][0]['address'], '3.2.3.2')
+		self.assertEqual(rep['controllers'][0]['port'], 7777)
